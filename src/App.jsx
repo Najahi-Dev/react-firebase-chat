cad6c33da2badbb009e2c8ac85fd7aefbe284c6b
ahmadh-najahi <ahmednajahi2003@gmail.com>
2025-05-14 01:11:40 +0530

    Add Some Designs and Styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
+import Chat from "./components/chats/Chat"
+import Detail from "./components/detail/Detail"
+import List from "./components/list/List"
+
 const App = () => {
   return (
-    <div className=''>App</div>
+    <div className='container'>
+      <List/>
+      <Chat/>
+      <Detail/>
+    </div>
   )
 }
 
---
+++ src/components/list/userinfo/Userinfo.jsx
@@ -0,0 +1,22 @@
+import "./userinfo.css";
+
+const Userinfo = () => {
+  return (
+    
+    <div className='userinfo'>
+        <div class="user">
+            <img src="./avatar.png" alt=""/>
+            <h2>John Doe</h2>
+        </div>
+        <div class="icons">
+            <img src="./more.png" alt=""/>
+            <img src="./video.png" alt=""/>
+            <img src="./edit.png" alt=""/>
+        </div>
+    </div>
+  )
+}
+
+
+export 
+default Userinfo
